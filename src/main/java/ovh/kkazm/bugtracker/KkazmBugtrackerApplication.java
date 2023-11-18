4447dd5d2ab12dac8de66091c4d83d6b4eede558
Konrad <89358750+kkazm@users.noreply.github.com>
2023-11-18 11:26:03 +0100

    Initial commit

---
+++ src/main/java/ovh/kkazm/bugtracker/KkazmBugtrackerApplication.java
@@ -0,0 +1,13 @@
+package ovh.kkazm.bugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkazmBugtrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkazmBugtrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ovh/kkazm/bugtracker/KkazmBugtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ovh.kkazm.bugtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkazmBugtrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
